7a4190da965e47532d09a10038aea3f3bcca6edc
gessica <gessicamohr18@gmail.com>
2021-02-06 20:55:47 -0800

    feeding options are working and they open new fragments

--- app/src/main/java/com/example/babymanager/FeedingActivity.java
+++ app/src/main/java/com/example/babymanager/FeedingActivity.java
@@ -1,16 +1,75 @@
 package com.example.babymanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class FeedingActivity extends AppCompatActivity {
+
+    private ImageView left_label, right_label, bottle_label, meal_label;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feeding);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //to create action for buttons to connect with fragments
+        init();
+
+        left_label.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.frame_layout, new LeftfeedingFragment());
+                ft.commit();
+            }
+        });
+
+        right_label.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.frame_layout, new RightfeedingFragment());
+                ft.commit();
+            }
+        });
+
+        bottle_label.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.frame_layout, new FormulafeedingFragment());
+                ft.commit();
+
+            }
+        });
+
+        meal_label.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.frame_layout, new SupfeedingFragment());
+                ft.commit();
+            }
+        });
+
+    }
+
+    private void init() {
+        left_label = findViewById(R.id.left_label);
+        right_label = findViewById(R.id.right_label);
+        bottle_label = findViewById(R.id.bottle_label);
+        meal_label = findViewById(R.id.meal_label);
 
     }
 }
---
+++ app/src/main/java/com/example/babymanager/FormulafeedingFragment.java
@@ -0,0 +1,31 @@
+package com.example.babymanager;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FormulafeedingFragment extends Fragment {
+
+    public FormulafeedingFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_formulafeeding, container, false);
+
+        return view;
+    }
+}
