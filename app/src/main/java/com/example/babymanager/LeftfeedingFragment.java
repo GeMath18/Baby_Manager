69af4fb242b11087c706464f1de57c6f8bdc3a2c
gessica <gessicamohr18@gmail.com>
2021-02-10 15:56:59 -0800

    implemented time and date to left feeding option fragment

--- app/src/main/java/com/example/babymanager/LeftfeedingFragment.java
+++ app/src/main/java/com/example/babymanager/LeftfeedingFragment.java
@@ -1,12 +1,21 @@
 package com.example.babymanager;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class LeftfeedingFragment extends Fragment {
@@ -16,8 +25,6 @@ public class LeftfeedingFragment extends Fragment {
     public LeftfeedingFragment() {
         // Required empty public constructor
     }
-
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,7 +38,53 @@ public class LeftfeedingFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_leftfeeding, container, false);
 
 
+        //date-time picker text label
+        EditText date_time_in = (EditText) view.findViewById(R.id.datetime_label);
+        date_time_in.setInputType(InputType.TYPE_NULL);
+
+        date_time_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateTimeDialog(date_time_in);
+            }
+
+        });
 
         return view;
+    }
+
+    private void showDateTimeDialog(EditText date_time_in) {
+
+        final Calendar calendar = Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener dateSetListener=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH,month);
+                calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+
+                TimePickerDialog.OnTimeSetListener timeSetListener=new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                        calendar.set(Calendar.MINUTE, minute);
+
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yy-MM-DD HH:mm");
+
+                        date_time_in.setText(simpleDateFormat.format(calendar.getTime()));
+
+                    }
+                };
+
+                new TimePickerDialog(getActivity(),timeSetListener,calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),false).show();
+
+            }
+        };
+
+        new DatePickerDialog(getActivity(),dateSetListener,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)).show();
     }
 }
