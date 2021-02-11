850f00c855e4b68882ef846b3f1f13440e36e114
gessica <gessicamohr18@gmail.com>
2021-02-11 15:13:55 -0800

    added diaper status spinner, it is working

--- app/src/main/java/com/example/babymanager/DiaperingActivity.java
+++ app/src/main/java/com/example/babymanager/DiaperingActivity.java
@@ -2,13 +2,88 @@ package com.example.babymanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class DiaperingActivity extends AppCompatActivity {
+
+    EditText date_time_in;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diapering);
+
+        EditText date_time_in = findViewById(R.id.datetime_diaper_label);
+        date_time_in.setInputType(InputType.TYPE_NULL);
+
+        date_time_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateTimeDialog(date_time_in);
+            }
+
+        });
+
+        Spinner spinner = findViewById(R.id.diaper_spinner);
+//        if (spinner != null) {
+//            spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) getActivity());
+//        }
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(DiaperingActivity.this,
+                R.array.diaper_status_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner.
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+    }
+
+    private void showDateTimeDialog(EditText date_time_in) {
+
+        final Calendar calendar = Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener dateSetListener=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH,month);
+                calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+
+                TimePickerDialog.OnTimeSetListener timeSetListener=new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                        calendar.set(Calendar.MINUTE, minute);
+
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yy-MM-dd HH:mm");
+
+                        date_time_in.setText(simpleDateFormat.format(calendar.getTime()));
+
+                    }
+                };
+
+                new TimePickerDialog(DiaperingActivity.this,timeSetListener,calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),false).show();
+
+            }
+        };
+
+        new DatePickerDialog(DiaperingActivity.this,dateSetListener,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)).show();
     }
 }
