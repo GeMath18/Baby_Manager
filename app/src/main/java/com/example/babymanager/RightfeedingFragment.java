c22ebfc4455a258c812e191cfc287a38a585995f
gessica <gessicamohr18@gmail.com>
2021-02-10 20:45:23 -0800

    date and time set up is working

--- app/src/main/java/com/example/babymanager/RightfeedingFragment.java
+++ app/src/main/java/com/example/babymanager/RightfeedingFragment.java
@@ -1,15 +1,35 @@
 package com.example.babymanager;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TimePicker;
+import android.widget.ToggleButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class RightfeedingFragment extends Fragment {
+
+    private Chronometer chronometer;
+    private long PauseOffset = 0;
+    private boolean isPlaying = false;
+    private ToggleButton toggleButton;
+    private ImageView reset_btn;
+    private Button save_l_button;
 
     public RightfeedingFragment() {
         // Required empty public constructor
@@ -26,9 +46,55 @@ public class RightfeedingFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_rightfeeding, container, false);
+        
 
+        //date-time picker text label
+        EditText date_time_in = (EditText) view.findViewById(R.id.datetime_r_label);
+        date_time_in.setInputType(InputType.TYPE_NULL);
 
+        date_time_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateTimeDialog(date_time_in);
+            }
+
+        });
 
         return view;
+    }
+
+    private void showDateTimeDialog(EditText date_time_in) {
+
+        final Calendar calendar = Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener dateSetListener=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH,month);
+                calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+
+                TimePickerDialog.OnTimeSetListener timeSetListener=new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                        calendar.set(Calendar.MINUTE, minute);
+
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yy-MM-DD HH:mm");
+
+                        date_time_in.setText(simpleDateFormat.format(calendar.getTime()));
+
+                    }
+                };
+
+                new TimePickerDialog(getActivity(),timeSetListener,calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),false).show();
+
+            }
+        };
+
+        new DatePickerDialog(getActivity(),dateSetListener,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)).show();
     }
 }
