fccc5fa3606e7402fcd6c02e6ca9d1da6771ae6b
gessica <gessicamohr18@gmail.com>
2021-02-12 22:52:57 -0800

    setting start and stop time for sleep is in sleeps view

--- app/src/main/java/com/example/babymanager/SleepActivity.java
+++ app/src/main/java/com/example/babymanager/SleepActivity.java
@@ -2,13 +2,151 @@ package com.example.babymanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class SleepActivity extends AppCompatActivity {
+
+    EditText date_start_time;
+    EditText end_time;
+    Button save_s_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //To set up Start time of sleep
+        EditText date_start_time = findViewById(R.id.datetime_start_input);
+        date_start_time.setInputType(InputType.TYPE_NULL);
+
+        date_start_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateTimeDialog(date_start_time);
+            }
+
+        });
+
+        //To set up End time of sleep
+        EditText end_time = findViewById(R.id.time_end_input);
+        end_time.setInputType(InputType.TYPE_NULL);
+
+        end_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimeDialog(end_time);
+            }
+        });
     }
+
+    //End Time Dialog
+    private void showTimeDialog(EditText end_time) {
+        final Calendar calendar = Calendar.getInstance();
+
+        TimePickerDialog.OnTimeSetListener timeSetListener= new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                calendar.set(Calendar.MINUTE, minute);
+
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("  HH:mm  ");
+
+                end_time.setText(simpleDateFormat.format(calendar.getTime()));
+
+            }
+        };
+
+        new TimePickerDialog(SleepActivity.this, timeSetListener,calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),false).show();
+    }
+
+
+    //Start time TimeDialog
+    private void showDateTimeDialog(EditText date_start_time) {
+
+        final Calendar calendar = Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener dateSetListener=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH,month);
+                calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+
+                TimePickerDialog.OnTimeSetListener timeSetListener=new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                        calendar.set(Calendar.MINUTE, minute);
+
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yy-MM-dd   HH:mm");
+
+                        date_start_time.setText(simpleDateFormat.format(calendar.getTime()));
+//
+                    }
+                };
+
+                new TimePickerDialog(SleepActivity.this,timeSetListener,calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),false).show();
+
+            }
+        };
+
+        new DatePickerDialog(SleepActivity.this,dateSetListener,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+
+    //Stop time TimeDialog
+//    private void showDateTimeDialog() {
+
+//        final Calendar calendar = Calendar.getInstance();
+//        DatePickerDialog.OnDateSetListener dateSetListener=new DatePickerDialog.OnDateSetListener() {
+//            @Override
+//            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//                calendar.set(Calendar.YEAR, year);
+//                calendar.set(Calendar.MONTH,month);
+//                calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+//
+//                TimePickerDialog.OnTimeSetListener timeSetListener=new TimePickerDialog.OnTimeSetListener() {
+//
+//                    @Override
+//                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+//                        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+//                        calendar.set(Calendar.MINUTE, minute);
+//
+//                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yy-MM-dd HH:mm");
+//
+////                        date_start_time.setText(simpleDateFormat.format(calendar.getTime()));
+//                        date_stop_time.setText(simpleDateFormat.format(calendar.getTime()));
+//                    }
+//                };
+//
+//                new TimePickerDialog(SleepActivity.this,timeSetListener,calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),false).show();
+//
+//            }
+//        };
+//
+//        new DatePickerDialog(SleepActivity.this,dateSetListener,
+//                calendar.get(Calendar.YEAR),
+//                calendar.get(Calendar.MONTH),
+//                calendar.get(Calendar.DAY_OF_MONTH)).show();
+//
+//
+//        showDateTimeDialog(date_stop_time);
+//    }
+
 }
